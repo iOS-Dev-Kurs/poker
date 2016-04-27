825fc555b51702d19e59d708e75d15a299dfb05d
XXXXX43 <logosalmac@dhcp568-170.laptop-wlc.uni-heidelberg.de>
2016-04-27 18:22:06 +0200

    enums implement

--- 04 - Poker.playground/Contents.swift
+++ 04 - Poker.playground/Contents.swift
@@ -7,11 +7,60 @@
 */
 /*
  EURE ANTWORT HIER
+ 1. only need one refernce to Card
+ 2. Enums eignen sich, weil es bei der Suit- und Rank-Auswahl mehrere Möglichkeiten gibt, welche sich gegenseitig ausschließen
 */
 
 
+enum Rank: Int {
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+    func RankDescription() -> String {
+        switch self {
+        case .Ace:
+            return "Ace"
+        case .King:
+            return "Jack"
+        case .Queen:
+            return "Queen"
+        case .Jack:
+            return "King"
+        case .Ten:
+            return "Ten"
+        case .Nine:
+            return "Nine"
+        case .Eight:
+            return "Eight"
+        case .Seven:
+            return "Seven"
+        case .Six:
+            return "Six"
+        case .Five:
+            return "Five"
+        case .Four:
+            return "Four"
+        case .Three:
+            return "Three"
+        case .Two:
+            return "Two"
+        }
+    }
+}
 
-
+enum Suit: Int {
+    case Spades, Hearts, Diamonds, Clubs
+    func SuitDescription() -> String {
+        switch self {
+        case .Spades:
+            return "U+2660"
+        case .Hearts:
+            return "U+2665"
+        case .Diamonds:
+            return "U+2666"
+        case .Clubs:
+            return "U+2663"
+        }
+    }
+}
 
 //: ## Testing
 /*
