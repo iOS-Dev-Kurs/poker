1e8106cb39f61051379896b9f4ed11e2af861d26
Kleimaier <vy442@r214c4.ad.uni-heidelberg.de>
2016-04-27 12:01:19 +0200

    On my way

--- 04 - Poker.playground/Contents.swift
+++ 04 - Poker.playground/Contents.swift
@@ -5,40 +5,46 @@
 
  - Wir modellieren `Card` als Struct, und `Suit` und `Rank` als Enums. Warum verwenden wir keine Klasse für `Card`? Warum eignet sich ein Enum so hervorragend für `Suit` und `Rank`?
 */
+import UIKit
+
+enum Rank: Int, CustomStringConvertible {
+    case two = 1, three = 2, four = 3, five = 4, six = 5, seven = 6, eight = 7, nine = 8, ten = 9, bube = 10, dame = 11, koenig = 12, ass = 13
+    var description: String {
+        switch self{
+        case .ass: return "A"
+        case .koenig: return "K"
+        case .dame: return "D"
+        case .bube: return "J"
+        case .ten: return "10"
+        case .nine: return "9"
+        case .eight: return "8"
+        case .seven: return "7"
+        case .six: return "6"
+        case .five: return "5"
+        case .four: return "4"
+        case .three: return "3"
+        case .two: return "2"
+        }
+    }
+}
+
+
+enum Suit: Int, CustomStringConvertible {
+    case Karo = 1, Herz = 2, Pik = 3, Kreuz = 4
+    var description: String{
+        switch self{
+        case .Karo: return "\u{2666}"
+        case .Herz: return "\u{2665}"
+        case .Pik: return "\u{2660}"
+        case .Kreuz: return "\u{2663}"
+        }
+    }
+}
+
 /*Wieso struct? Weil Karten statische Dinge sind*/
 struct Card: CustomStringConvertible {
     /*Wieso enum? Weil sich die Faelle gegenseitig ausschließen und nur eine Karte eine Farbe und einen Rang haben kann */
-    enum Suit: Int, CustomStringConvertible {
-        case Karo = 1, Herz = 2, Pik = 3, Kreuz = 4
-        var description: String{
-            switch self{
-            case .Karo: return "\u{2666}"
-            case .Herz: return "\u{2665}"
-            case .Pik: return "\u{2660}"
-            case .Kreuz: return "\u{2663}"
-            }
-        }
-    }
-    enum Rank: Int, CustomStringConvertible {
-        case two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, eight = 8, nine = 9, ten = 10, bube = 11, dame = 12, koenig = 13, ass = 14
-        var description: String {
-            switch self{
-            case .ass: return "A"
-            case .koenig: return "K"
-            case .dame: return "D"
-            case .bube: return "J"
-            case .ten: return "10"
-            case .nine: return "9"
-            case .eight: return "8"
-            case .seven: return "7"
-            case .six: return "6"
-            case .five: return "5"
-            case .four: return "4"
-            case .three: return "3"
-            case .two: return "2"
-            }
-        }
-    }
+    
     let suit: Suit
     let rank: Rank
     
@@ -46,23 +52,35 @@ struct Card: CustomStringConvertible {
     return "\(suit) \(rank)"
     }
     
-
 }
 
-struct PokerHand {
+extension Card: Equatable{}
+func ==(lhs: Card, rhs: Card) -> Bool {
+    return lhs.suit == rhs.suit && lhs.rank == rhs.rank
+}
+
+struct PokerHand{
     let cards: [Card]
     
     var description: String{
         return "\(cards)"
     }
     
-    /*init(cards: [Card]){
-    }*/
+    init(cards: [Card]){
+        for index in 1...5 {
+            let rndSuit = Suit(rawValue: Int(arc4random_uniform(4)))!
+            let rndRank = Rank(rawValue: Int(arc4random_uniform(13)))!
+            let rndCard = Card(suit: rndSuit, rank: rndRank)
+            self.cards = [rndCard]
+        }
+        
+    }
 }
+
 
 let card = Card(suit: .Herz,rank: .ass)
 print(card)
-
+print("Hello World!")
 
 //: ## Testing
 /*
