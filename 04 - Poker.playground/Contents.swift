4c2484bc67b8900da6ae3400feea645091b0b74c
bippo646 <vi438@r214c7.ad.uni-heidelberg.de>
2016-04-27 18:22:45 +0200

    test

--- 04 - Poker.playground/Contents.swift
+++ 04 - Poker.playground/Contents.swift
@@ -7,8 +7,57 @@
 */
 /*
  EURE ANTWORT HIER
-*/
-
+ 
+ Enums verwenden wir, da die Einträge von Suit und Rank immer nur 4 bzw. 13 Möglichkeiten bieten.
+ */
+enum Suit: Int{
+    case Diamonds, Spades, Hearts, Clubs
+    func suitDescription() -> String{
+        switch self{
+            case .Diamonds:
+                return "diamond"
+            case .Spades:
+                return "spades"
+            case .Hearts:
+                return "hearts"
+            case .Clubs:
+                return "clubs"
+        }
+    }
+}
+enum Rank: Int{
+    case two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace
+    func rankDescription() -> String{
+        switch self{
+        case .two:
+            return "two"
+        case .three:
+            return "three"
+        case .four:
+            return "four"
+        case .five:
+            return "five"
+        case .six:
+            return "six"
+        case .seven:
+            return "seven"
+        case .eight:
+            return "eight"
+        case .nine:
+            return "nine"
+        case .ten:
+            return "ten"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        case .ace:
+            return "ace"
+        }
+    }
+}
 
 
 
