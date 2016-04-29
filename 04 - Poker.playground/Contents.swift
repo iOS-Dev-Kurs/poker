0e9d4a1eead972996496f792932b4d5b8011a6e1
julian libor <gb004@r214c3.ad.uni-heidelberg.de>
2016-04-29 12:51:20 +0200

    zwischenspeichern
    Deck wird erstellt, es fehlt noch der Feinschliff

--- 04 - Poker.playground/Contents.swift
+++ 04 - Poker.playground/Contents.swift
@@ -5,10 +5,95 @@
 
  - Wir modellieren `Card` als Struct, und `Suit` und `Rank` als Enums. Warum verwenden wir keine Klasse für `Card`? Warum eignet sich ein Enum so hervorragend für `Suit` und `Rank`?
 */
+
+
 /*
  EURE ANTWORT HIER
 */
 
+
+
+import Foundation
+
+
+enum Suit: Int, CustomStringConvertible {
+    case Hearts, Diamonds, Spades, Clubs  //Herz ♥, Karo ♦, Pike ♠, Kreuz ♣
+    var description: String {
+        switch self {
+        case .Hearts: return "♥"
+        case .Diamonds: return "♦"
+        case .Spades: return "♠"
+        case .Clubs: return "♣"
+        }
+    }
+}
+
+enum Rank: Int, CustomStringConvertible {
+    case one, two, three, four, five, six, seven, eight, nine, ten, Jack, Queen, King, Ace
+    
+    var description: String {
+        switch self {
+        case .one: return "one"
+        case .two: return "two"
+        case .three: return "three"
+        case .four: return "four"
+        case .five: return "five"
+        case .six: return "six"
+        case .seven: return "seven"
+        case .eight: return "eight"
+        case .nine: return "nine"
+        case .ten: return "ten"
+        case .Jack: return "jack"
+        case .Queen: return "Queen"
+        case .King: return "King"
+        case .Ace:  return "Ace"
+        }
+    }
+}
+
+struct Card: CustomStringConvertible {
+    let suit: Suit
+    let rank: Rank
+    
+    var description: String {
+        return "\(suit), \(rank)"
+    }
+}
+
+extension Card: Equatable {}
+
+func==(lhs: Card, rhs: Card) -> Bool {
+    return lhs.suit == rhs.suit && lhs.rank == rhs.rank
+}
+
+struct PokerHand {
+    var cards: [Card] = []
+    
+    init (){
+        while cards.count < 5 {
+            let rndSuit = Suit(rawValue: Int(arc4random_uniform(4)))!
+            let rndRank = Rank(rawValue: Int(arc4random_uniform(13)))!
+            let rndCard = Card(suit: rndSuit, rank: rndRank)
+            if !cards.contains(rndCard) {
+                cards.append(rndCard)
+
+            }
+        }
+       
+    }
+    
+    var description: String {
+        var out = ""
+        for card in cards {
+            out += "\(card.suit), \(card.rank) "
+        }
+        return out
+    }
+    
+}
+var test = PokerHand()
+var zwei = PokerHand()
+print (zwei.description)
 
 
 
